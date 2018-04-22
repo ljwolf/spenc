72b205326dccc2321f724e6cceaab281201b707d
ljwolf <levi.john.wolf@gmail.com>
2018-04-22 23:56:18 +0100

    use built-in sklearn random_state as well as numpy seed

--- gendata.py
+++ gendata.py
@@ -16,27 +16,27 @@ if __name__ == "__main__":
 
     print('(1 of 5) doing 10k nodata')
     np.random.seed(SEED)
-    labels = spenc.SPENC(n_clusters=10).fit(None, natR.sparse).labels_
+    labels = spenc.SPENC(n_clusters=10, random_state=SEED).fit(None, natR.sparse).labels_
     labels.dump(os.path.join(datadir, 'nat_10k_nodata.ary'))
 
     print('(2 of 5) doing 30k sampling')
     np.random.seed(SEED)
-    labels = spenc.SPENC(n_clusters=30).sample(natR.sparse, n_samples=3)
+    labels = spenc.SPENC(n_clusters=30, random_state=SEED).sample(natR.sparse, n_samples=3)
     labels.dump(os.path.join(datadir, 'nat_30k_randoms.ary'))
 
     print('(3 of 5) doing 30k withdata')
     np.random.seed(SEED)
-    labels = spenc.SPENC(n_clusters=30, gamma = .001).fit(X, natR.sparse).labels_
+    labels = spenc.SPENC(n_clusters=30, gamma=.001, random_state=SEED).fit(X, natR.sparse).labels_
     labels.dump(os.path.join(datadir, 'nat_30k_discovered.ary'))
 
     print('(4 of 5) doing infk sampling')
     np.random.seed(SEED)
-    labels = spenc.SPENC(n_clusters=np.inf).sample(natR.sparse, floor=20)
+    labels = spenc.SPENC(n_clusters=np.inf, random_state=SEED).sample(natR.sparse, floor=20)
     labels.dump(os.path.join(datadir, 'nat_infk_randoms.ary'))
 
     print('(5 of 5) doing infk withdata')
     np.random.seed(SEED)
-    labels = spenc.SPENC(n_clusters=np.inf, gamma=.001).fit(X, natR.sparse, floor=20).labels_
+    labels = spenc.SPENC(n_clusters=np.inf, gamma=.001, random_state=SEED).fit(X, natR.sparse, floor=20).labels_
     labels.dump(os.path.join(datadir, 'nat_infk_discovered.ary'))
 
 
--- spenc/tests/test_spenc.py
+++ spenc/tests/test_spenc.py
@@ -4,11 +4,12 @@ import numpy as np
 import os
 import geopandas as gpd
 import scipy.sparse.csgraph as csg
+from sklearn.metrics import accuracy_score
 
-from spenc.utils import lattice
-import spenc 
+import spenc
 
 filepath = os.path.dirname(__file__)
+SEED = 1901
 
 
 class SPENCTest(TestCase):
@@ -27,7 +28,7 @@ class SPENCTest(TestCase):
 
     def test_NAT_nodata(self):
         np.random.seed(1901) #shouldn't matter substantively, only for label#
-        t1 = spenc.SPENC(n_clusters=10).fit(None, self.natR.sparse).labels_
+        t1 = spenc.SPENC(n_clusters=10, random_state=SEED).fit(None, self.natR.sparse).labels_
         for label in range(t1.max()):
             mask = t1 == label
             subgraph = self.natR.sparse[mask,:][:,mask]
@@ -35,11 +36,11 @@ class SPENCTest(TestCase):
             n_components, labels = csg.connected_components(subgraph)
             self.assertEqual(n_components, 1,
                              'Disconnected component ({}) in NAT clusters!'.format(label))
-        np.testing.assert_array_equal(t1, self.nat_10k_nodata)
+        self.assertEqual(accuracy_score(t1, self.nat_10k_nodata), 1.0)
 
     def test_NAT_randoms(self):
         np.random.seed(1901)
-        randoms = spenc.SPENC(n_clusters=30).sample(self.natR.sparse, n_samples=3)
+        randoms = spenc.SPENC(n_clusters=30, random_state=SEED).sample(self.natR.sparse, n_samples=3)
         self.assertEqual(randoms.shape, (3, len(self.nat)), 'sample shapes are incorrect!')
         for i,random in enumerate(randoms):
             for label in range(random.max()):
@@ -50,9 +51,9 @@ class SPENCTest(TestCase):
                 self.assertEqual(n_components, 1,
                                  'Disconnected component ({}) in NAT '
                                  'random cluster set {}!'.format(label,i) )
-        np.testing.assert_array_equal(randoms, self.nat_30k_randoms)
+            self.assertEqual(accuracy_score(random, self.nat_30k_randoms[i]), 1.0)
         np.random.seed(1901)
-        randoms = spenc.SPENC(n_clusters=np.inf).sample(self.natR.sparse, floor=20)
+        randoms = spenc.SPENC(n_clusters=np.inf, random_state=SEED).sample(self.natR.sparse, floor=20)
         self.assertEqual(randoms.shape, (len(self.nat),), 'sample shapes are incorrect!')
         for label in range(randoms.max()):
             mask = randoms == label
@@ -62,11 +63,12 @@ class SPENCTest(TestCase):
             self.assertEqual(n_components, 1,
                              'Disconnected component ({}) in NAT '
                              'random cluster set {}!'.format(label,i))
-        np.testing.assert_array_equal(randoms, self.nat_infk_randoms)
+        #remember, this is only one draw
+        self.assertEqual(accuracy_score(randoms, self.nat_infk_randoms), 1.0)
 
     def test_NAT_data(self):
         np.random.seed(1901)
-        k30 = spenc.SPENC(n_clusters=30, gamma=.001).fit(self.natX, self.natR.sparse)
+        k30 = spenc.SPENC(n_clusters=30, gamma=.001, random_state=SEED).fit(self.natX, self.natR.sparse)
         for label in range(k30.labels_.max()):
             mask = k30.labels_ == label
             subgraph = self.natR.sparse[mask,:][:,mask]
@@ -74,9 +76,9 @@ class SPENCTest(TestCase):
             n_components, labels = csg.connected_components(subgraph)
             self.assertEqual(n_components, 1,
                              'Disconnected component ({}) in NAT clusters!'.format(label))
-        np.testing.assert_array_equal(k30.labels_, self.nat_30k_discovered)
+        self.assertEqual(accuracy_score(k30.labels_, self.nat_30k_discovered), 1.0)
         np.random.seed(1901)
-        kinf = spenc.SPENC(n_clusters=np.inf, gamma=.001)\
+        kinf = spenc.SPENC(n_clusters=np.inf, gamma=.001, random_state=SEED)\
                     .fit(self.natX, self.natR.sparse, floor=20)
         for label in range(kinf.labels_.max()):
             mask = kinf.labels_ == label
@@ -85,4 +87,4 @@ class SPENCTest(TestCase):
             n_components, labels = csg.connected_components(subgraph)
             self.assertEqual(n_components, 1,
                              'Disconnected component ({}) in NAT clusters!'.format(label))
-        np.testing.assert_array_equal(kinf.labels_, self.nat_infk_discovered)
+        self.assertEqual(accuracy_score(kinf.labels_, self.nat_infk_discovered), 1.0)
