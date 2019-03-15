29e12f556ca5cef4e2963dfd5d86e096c8ec461b
ljwolf <levi.john.wolf@gmail.com>
2019-03-15 08:15:55 +0000

    add version in init

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup
+import os
+
+basepath = os.path.dirname(__file__)
+init = os.path.join(basepath, 'spenc/__init__.py')
+
+with open(init, 'r') as initfile:
+    firstline = initfile.readline()
+init_version = firstline.split('=')[-1].strip()
 
 setup(name='spenc',
-      version='0.2',
+      version=init_version,
       description='Spatially-Encouraged Spectral Clustering, a method of discovering clusters/deriving labels for spatially-referenced data with attribute/labels attached',
       url='https://github.com/ljwolf/spenc',
       author='Levi John Wolf',
--- spenc/__init__.py
+++ spenc/__init__.py
@@ -1,1 +1,3 @@
+__version__ = '0.3.0dev'
+
 from .abstracts import SPENC
